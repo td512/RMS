5a27076112abdaf07a0dd5bbc83c0bec97e07ced
td512 <td512@users.noreply.github.com>
2017-08-16 14:09:07 +1200

    updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,16 @@
 class PostsController < ApplicationController
+  def post
+    post = Post.new()
+    post.owner = current_user.first_name
+    post.post_title = params[:write][:title]
+    post.post_subtitle = params[:write][:subtitle]
+    post.post_body = params[:write][:content]
+    post.post_category = params[:write][:tags]
+    post.slug = params[:write][:title].parameterize
+    if post.save
+      redirect_to post_path(post.id, post.slug)
+    end
+  end
   def details
     if Post.first.nil?
       if Post.first.nil? && params[:id].present?
