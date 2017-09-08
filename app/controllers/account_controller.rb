2c3d6502b01230f637d01d98c3b8fd83d6f5b765
td512 <td512@users.noreply.github.com>
2017-09-09 01:33:54 +1200

    fix account

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -5,5 +5,20 @@ class AccountController < ApplicationController
   def profile
   end
   def update
+  if params[:profile][:first_name].present?
+    current_user.first_name = params[:profile][:first_name]
+  end
+    if params[:profile][:last_name].present?
+      current_user.last_name = params[:profile][:last_name]
+    end
+      if params[:profile][:email].present?
+        current_user.email = params[:profile][:email]
+      end
+        if params[:profile][:password].present?
+          current_user.password = params[:profile][:password]
+        end
+        if current_user.save
+          redirect_to account_path
+      end
   end
 end
