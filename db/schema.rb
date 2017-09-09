c27a4192114579015c9d206e28ba26459b2126e2
td512 <td512@users.noreply.github.com>
2017-09-10 00:32:45 +1200

    Comments!

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170907012903) do
     t.string   "awaiting_moderation"
     t.string   "post_id"
     t.string   "deleted"
+    t.string   "content"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
   end
