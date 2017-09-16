d5ef36348385b059d58bef8118eae96c9758ff71
td512 <td512@users.noreply.github.com>
2017-09-17 11:41:25 +1200

    Revert "demo init"
    This reverts commit 15f1e68ad96a5eaffa5e0688beee404d084d1dc0.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :password, length: { minimum: 8 }, allow_nil: false, on: :create
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,37 @@ Rails.application.routes.draw do
   # Login
 scope '/login' do
   get '/'  => 'sessions#new', as: :login
+  get '/error'  => 'sessions#error', as: :login_error
+  get '/error/passwd'  => 'sessions#newpass', as: :login_passwd
+  get '/session'  => 'sessions#loggedout', as: :login_session
+  post '/'  => 'sessions#create'
+end
+# Logout
+scope '/logout' do
+  get '/' => 'sessions#destroy', as: :logout
+end
+# Reset
+scope '/reset' do
+  get '/' => 'reset#new', as: :reset
+  post '/' => 'reset#check'
+  get '/error' => 'reset#error', as: :reset_error
+  get '/change/error' => 'reset#chgerror', as: :reset_error_reset
+  get '/done' => 'reset#done', as: :reset_done
+  get '/:id' => 'reset#reset'
+  post '/:id' => 'reset#change', as: :reset_reset
+end
+# Signup
+scope '/signup' do
+  get '/'  => 'users#new', as: :signup
+  get '/error/email'  => 'users#emlerr', as: :emlerr
+  get '/error/user'  => 'users#usrerr', as: :usrerr
+  post '/'  => 'users#create'
+end
+# Verify
+scope '/verify' do
+  get '/' => 'verify#new', as: :verify
+  post '/' => 'verify#verify'
+  get '/error' => 'verify#error', as: :verify_error
 end
 # Settings
 scope '/settings' do
@@ -18,6 +49,18 @@ scope '/write' do
 end
 scope '/manage' do
   get '/' => 'posts#manage', as: :manage
+  scope '/links' do
+    get '/' => 'dashboard#link', as: :link
+    get '/add' => 'dashboard#addlink', as: :addlink
+    post '/add' => 'dashboard#linkadd'
+    get '/delete/:id' => 'dashboard#dellink', as: :linkdelete
+  end
+  scope '/menus' do
+    get '/' => 'dashboard#menu', as: :menu
+    get '/add' => 'dashboard#addmenu', as: :addmenu
+    post '/add' => 'dashboard#menuadd'
+    get '/delete/:id' => 'dashboard#delmenu', as: :menudelete
+  end
   get '/delete/:id' => 'posts#delete', as: :delete
 end
 # Posts
@@ -33,6 +76,9 @@ scope '/posts' do
 end
 scope '/users' do
   get '/manage' => 'dashboard#usrmgmt', as: :usrmgmt
+  get '/make/admin/:id' => 'account#mkadmin', as: :mkadmin
+  get '/make/user/:id' => 'account#mkusr', as: :mkusr
+  get '/deactivate/:id' => 'account#deactivate', as: :deactivate
   get '/impersonate/:id' => 'account#impersonate', as: :impersonate
   get '/stop-impersonation/:id' => 'account#stop_impersonate', as: :stopimpersonate
 end
